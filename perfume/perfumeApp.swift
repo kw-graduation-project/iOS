47f13277a52a3668a9b3b3da2f458fa8e1a64709
jay <jay.kim@bclabs.co.kr>
2022-04-07 17:14:22 +0900

    add initial xcode project

---
+++ perfume/perfumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  perfumeApp.swift
+//  perfume
+//
+//  Created by jaehan kim on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct perfumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
